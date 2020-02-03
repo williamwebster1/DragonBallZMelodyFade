8ece98f713b62c78157811e5eb0d18eb11e75f02
williamwebster1 <37878196+williamwebster1@users.noreply.github.com>
2020-02-03 13:57:19 -0800

    Add files via upload

---
+++ fadedbz.rb
@@ -0,0 +1,108 @@
+sample "C:/Users/william_webster/Desktop/im_ready.wav"
+sleep 7.2
+use_bpm 162
+play :G5
+play :G4
+sleep 1
+play :A5
+play :A4
+sleep 1
+play :G5
+play :G4
+sleep 1
+play :A5
+play :A4
+sleep 1
+
+play :G5
+play :G4
+sleep 1
+play :F4
+play :F5
+sleep 0.5
+play :E4
+play :E5
+sleep 1
+play :D4
+play :D5
+sleep 1.2
+
+play :F4
+play :F5
+sleep 1
+play :E4
+play :E5
+sleep 1
+play :F4
+play :F5
+sleep 1
+
+play :E4
+play :E5
+sleep 1
+play :D4
+play :D5
+sleep 1
+play :C4
+play :C5
+sleep 1
+play :B3
+play :B4
+
+sleep 1
+play :A3
+play :A4
+sleep 1
+play :C4
+play :C5
+sleep 1
+play :F4
+play :F5
+
+sleep 1
+play :E4
+play :E5
+sleep 1
+play :G4
+play :G5
+sleep 0.5
+play :C4
+play :C5
+
+sleep 1
+play :D4
+play :D5
+sleep 0.7
+play :E4
+play :E5
+sleep 0.7
+play :F4
+play :F5
+sleep 0.7
+play :D4
+play :D5
+sleep 0.7
+play :E4
+play :E5
+sleep 0.7
+play :F4
+play :F5
+
+sleep 1
+play :G4, amp: 0.75
+play :B4, amp: 0.75
+play :D5, amp: 0.75
+play :G5, amp: 0.75
+sleep 1
+play :G4, amp: 0.5
+play :B4, amp: 0.5
+play :D5, amp: 0.5
+play :G5, amp: 0.5
+sleep 1
+play :A4, amp: 0.25
+play :A5, amp: 0.25
+sleep 1
+play :B3, amp: 0.15
+play :B4, amp: 0.15
+sleep 0.5
+play :C4, amp: 0.10
